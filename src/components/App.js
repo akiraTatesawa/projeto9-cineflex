80c3e8e6a13d0803993e720a20f6efe7d0ff145d
Arthur Akira <arthurak10.at@gmail.com>
2022-05-17 12:14:44 -0300

    Layout: Screen 2

--- src/components/App.js
+++ src/components/App.js
@@ -2,7 +2,7 @@ import "../assets/styles/reset.css";
 import "../assets/styles/styles.css";
 import Footer from "./Footer";
 import MovieDayHour from "./MovieDayHour";
-import MoviesSection from "./MoviesSection";
+// import MoviesSection from "./MoviesSection";
 
 export default function App() {
   return (
--- src/components/MovieDayHour.js
+++ src/components/MovieDayHour.js
@@ -1,3 +1,27 @@
-export default function MovieDayHour () {
-    
+function MovieData() {
+  return (
+    <>
+      <h3>Quinta-feira - 24/06/2021</h3>
+      <div className="hour-buttons">
+        <button>15:00</button>
+        <button>19:00</button>
+        <button>19:00</button>
+        <button>19:00</button>
+        <button>19:00</button>
+      </div>
+    </>
+  );
+}
+
+export default function MovieDayHour() {
+  return (
+    <>
+      <h2>Selecione o horário</h2>
+      <section className="date-selection">
+        <MovieData />
+        <MovieData />
+        <MovieData />
+      </section>
+    </>
+  );
 }
